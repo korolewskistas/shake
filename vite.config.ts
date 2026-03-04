e185f1b59bf0d586fa6d938d1e47f7db07f346e9
korolewskistas <korolewskistas@gmail.com>
2026-03-04 12:33:08 +0700

    Build for GitHub Pages and update logo position

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  build: {
+    outDir: 'docs',
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
